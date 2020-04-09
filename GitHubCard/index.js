057c0d5c1dc5eccad4f420fd96b5430f0f5d641b
toricruzmendiola <mtoricruz@gmail.com>
2020-04-09 13:00:59 -0700

    started project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,11 @@
+import axios from 'axios'
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/mtoricruz')
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
